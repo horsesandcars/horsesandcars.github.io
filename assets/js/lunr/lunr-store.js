a4c1d07dc6ccbfd7f7153135ac0b9eeccbf7c598
ChrisHammond <ChrisHammond@users.noreply.github.com>
2020-12-23 18:15:20 +0000

    deploy: e0c0715b4d048948f5eb7c67c1b9864be6afecb3

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,19 +1,7 @@
 var store = [{
-        "title": "Welcome to TheTrashman.com!",
-        "excerpt":"You’ve found a great domain that is for sale. If you have a trash or refuse business, or any other type of business this domain might be perfect for you. If you are interested in purchasing the domain fill out the contact form on this page. Include your best offer...","categories": [],
+        "title": "Welcome To Horses and Cars - The Hammond Family Website",
+        "excerpt":"Find each of us online:   Chris   Natalie   Jacque   Daniel  ","categories": [],
         "tags": [],
-        "url": "/welcome-to-thetrashman-com",
-        "teaser": null
-      },{
-        "title": "TheTrashman.com in Rocket League!",
-        "excerpt":"TheTrashman.com is a proud sponsor of Chris Hammond’s eSports gaming efforts. You can currently find him playing in competitive Rocket League Matches.   You can also find him playing Solo/Duals/Trios/Squads in Fortnite.   Did you get beat by TheTrashman.com? Did you beat him? Let us know!  ","categories": [],
-        "tags": [],
-        "url": "/thetrashman-com-in-rocket-league",
-        "teaser": null
-      },{
-        "title": "TheTrashman.com Rocket League Game Play!",
-        "excerpt":"The latest replay video from TheTrashman.com Rocket League  ","categories": [],
-        "tags": [],
-        "url": "/thetrashman-video-replay",
+        "url": "/welcome-to-horses-and-cars",
         "teaser": null
       }]
--- banner.js
+++ banner.js
@@ -8,7 +8,9 @@ const date = `${dateObj.getFullYear()}-${padStart(
   dateObj.getMonth() + 1
 )}-${padStart(dateObj.getDate())}`;
 const banner = `/*!
- * Another website by Christoc.com
+ * Minimal Mistakes Jekyll Theme ${pkg.version} by ${pkg.author}
+ * Copyright 2013-${dateObj.getFullYear()} Michael Rose - mademistakes.com | @mmistakes
+ * Licensed under ${pkg.license}
  */
 `;
 

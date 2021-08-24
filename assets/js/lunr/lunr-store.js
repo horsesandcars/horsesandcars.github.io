0a1e27fc7776d37cd85a7662ccfd9ef989e5d811
ChrisHammond <ChrisHammond@users.noreply.github.com>
2021-08-24 19:09:12 +0000

    deploy: 52db49d57de98e181297aea185df25a78f4d2a03

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Welcome To Horses and Cars - The Hammond Family Website",
+        "excerpt":"Find each of us online:   Chris   Natalie   Jacque   Daniel  ","categories": [],
+        "tags": [],
+        "url": "/welcome-to-horses-and-cars",
+        "teaser": null
+      }]
